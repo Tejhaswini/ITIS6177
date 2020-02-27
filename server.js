6325c5b56a2fc5ecf0ad56d4ce7a8cb8720b23ce
tejhaswini <tejhaswini.atluri@gmail.com>
2020-02-26 22:09:40 -0500

    Eight Commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const axios = require("axios");
-const port = 3000
+const port = 3001
 app.use(cors());
 
 app.get('/', (req, res) => {
